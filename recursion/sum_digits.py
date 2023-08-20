cbb4296358123c8c11b3ad701e463ea496c3f305
Omid Roshani <omidroshanie@gmail.com>
2023-08-20 15:30:09 +0330

    practice  recursion with sum digits in integer number question

---
+++ recursion/sum_digits.py
@@ -0,0 +1,9 @@
+def sum_digits_in_integer(integer):
+    assert integer>=0 and int(integer) == integer, "You must enter integer and positive number"
+    first_remainder = integer % 10
+    if integer == 0:
+        return 0
+    else:
+        return first_remainder + sum_digits_in_integer(int(integer/10))
+    
+print(sum_digits_in_integer(129))
